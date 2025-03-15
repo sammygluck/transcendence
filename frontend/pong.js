b3507a95ba499f65e6e8317d2404dd412c8e0f76
joeri <143255616+jnijs@users.noreply.github.com>
2025-03-15 17:28:56 +0100

    add tsconfig.json for typescript compiling

--- frontend/pong.js
+++ frontend/pong.js
@@ -1,6 +1,11 @@
-// all numbers are in % of screen height, if the aspect ratio is fixed to 2, this means that the x axis goes to 200%
-var Paddle = /** @class */ (function () {
-    function Paddle(x, y, width, height, speed) {
+class Paddle {
+    x;
+    y;
+    width;
+    height;
+    speed;
+    dy;
+    constructor(x, y, width, height, speed) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -8,7 +13,7 @@ var Paddle = /** @class */ (function () {
         this.speed = speed;
         this.dy = 0;
     }
-    Paddle.prototype.move = function (duration) {
+    move(duration) {
         this.y += this.dy * duration / 1000;
         if (this.y < 0) {
             this.y = 0;
@@ -16,64 +21,63 @@ var Paddle = /** @class */ (function () {
         else if (this.y + this.height > 100) {
             this.y = 100 - this.height;
         }
-    };
-    Paddle.prototype.draw = function (ctx, canvasHeight) {
-        var scaleFactor = canvasHeight / 100;
+    }
+    draw(ctx, canvasHeight) {
+        let scaleFactor = canvasHeight / 100;
         ctx.fillStyle = "white";
         ctx.fillRect(this.x * scaleFactor, this.y * scaleFactor, this.width * scaleFactor, this.height * scaleFactor);
-    };
-    return Paddle;
-}());
-var Ball = /** @class */ (function () {
-    function Ball(x, y, radius, speed) {
+    }
+}
+class Ball {
+    x;
+    y;
+    radius;
+    speedX;
+    speedY;
+    constructor(x, y, radius, speed) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.speedX = speed;
         this.speedY = speed;
     }
-    Ball.prototype.move = function (duration) {
+    move(duration) {
         this.x += this.speedX * duration / 1000;
         this.y += this.speedY * duration / 1000;
         if ((this.y - this.radius <= 0 && this.speedY < 0) || (this.y + this.radius >= 100 && this.speedY > 0)) {
             this.speedY *= -1;
         }
-    };
-    Ball.prototype.checkCollision = function (paddle) {
-        // we may have to add collision detection with the top and bottom of the paddle
-        // y direction
+    }
+    checkCollision(paddle) {
         if (this.y >= paddle.y && this.y <= paddle.y + paddle.height) {
-            // x direction left paddle
             if (this.speedX < 0 && paddle.x < 50 && this.x - this.radius <= paddle.x + paddle.width) {
                 this.speedX *= -1;
             }
-            // x direction right paddle
             else if (this.speedX > 0 && paddle.x > 50 && this.x + this.radius >= paddle.x) {
                 this.speedX *= -1;
             }
         }
-        // with the code below, the ball was sticking to the paddle sometimes
-        /*
-        if ( this.x - this.radius <= paddle.x + paddle.width && this.x + this.radius >= paddle.x &&
-            this.y >= paddle.y && this.y <= paddle.y + paddle.height )
-        {
-            this.speedX *= -1;
-        }
-            */
-    };
-    Ball.prototype.draw = function (ctx, canvasHeight) {
-        var scaleFactor = canvasHeight / 100;
+    }
+    draw(ctx, canvasHeight) {
+        let scaleFactor = canvasHeight / 100;
         ctx.fillStyle = "white";
         ctx.beginPath();
         ctx.arc(this.x * scaleFactor, this.y * scaleFactor, this.radius * scaleFactor, 0, Math.PI * 2);
         ctx.fill();
-    };
-    return Ball;
-}());
-var Game = /** @class */ (function () {
-    function Game(canvas) {
+    }
+}
+class Game {
+    canvas;
+    ctx;
+    paddleLeft;
+    paddleRight;
+    ball;
+    scoreLeft;
+    scoreRight;
+    prevTime;
+    constructor(canvas) {
         this.canvas = canvas;
-        this.ctx = canvas.getContext("2d"); //need explantion
+        this.ctx = canvas.getContext("2d");
         this.paddleLeft = new Paddle(1, 45, 2, 10, 30);
         this.paddleRight = new Paddle(197, 45, 2, 10, 30);
         this.ball = new Ball(100, 50, 1, 30);
@@ -83,38 +87,36 @@ var Game = /** @class */ (function () {
         this.handleInput();
         this.loop(0);
     }
-    Game.prototype.handleInput = function () {
-        var _this = this;
-        window.addEventListener("keydown", function (e) {
+    handleInput() {
+        window.addEventListener("keydown", (e) => {
             if (e.key === "w")
-                _this.paddleLeft.dy = -_this.paddleLeft.speed;
+                this.paddleLeft.dy = -this.paddleLeft.speed;
             if (e.key === "s")
-                _this.paddleLeft.dy = _this.paddleLeft.speed;
+                this.paddleLeft.dy = this.paddleLeft.speed;
             if (e.key === "ArrowUp")
-                _this.paddleRight.dy = -_this.paddleRight.speed;
+                this.paddleRight.dy = -this.paddleRight.speed;
             if (e.key === "ArrowDown")
-                _this.paddleRight.dy = _this.paddleRight.speed;
+                this.paddleRight.dy = this.paddleRight.speed;
         });
-        window.addEventListener("keyup", function (e) {
-            if (e.key === "w" && _this.paddleLeft.dy < 0)
-                _this.paddleLeft.dy = 0;
-            if (e.key === "s" && _this.paddleLeft.dy > 0)
-                _this.paddleLeft.dy = 0;
-            if (e.key === "ArrowUp" && _this.paddleRight.dy < 0)
-                _this.paddleRight.dy = 0;
-            if (e.key === "ArrowDown" && _this.paddleRight.dy > 0)
-                _this.paddleRight.dy = 0;
+        window.addEventListener("keyup", (e) => {
+            if (e.key === "w" && this.paddleLeft.dy < 0)
+                this.paddleLeft.dy = 0;
+            if (e.key === "s" && this.paddleLeft.dy > 0)
+                this.paddleLeft.dy = 0;
+            if (e.key === "ArrowUp" && this.paddleRight.dy < 0)
+                this.paddleRight.dy = 0;
+            if (e.key === "ArrowDown" && this.paddleRight.dy > 0)
+                this.paddleRight.dy = 0;
         });
-    };
-    Game.prototype.loop = function (timestamp) {
-        var _this = this;
+    }
+    loop(timestamp) {
         if (timestamp - this.prevTime < 1000)
             this.update(timestamp - this.prevTime);
         this.render();
         this.prevTime = timestamp;
-        requestAnimationFrame(function (timestamp) { return _this.loop(timestamp); });
-    };
-    Game.prototype.update = function (duration) {
+        requestAnimationFrame((timestamp) => this.loop(timestamp));
+    }
+    update(duration) {
         this.paddleLeft.move(duration);
         this.paddleRight.move(duration);
         this.ball.move(duration);
@@ -128,13 +130,13 @@ var Game = /** @class */ (function () {
             this.scoreLeft++;
             this.resetBall();
         }
-    };
-    Game.prototype.resetBall = function () {
+    }
+    resetBall() {
         this.ball.x = 100;
         this.ball.y = 50;
         this.ball.speedX = -this.ball.speedX;
-    };
-    Game.prototype.render = function () {
+    }
+    render() {
         this.ctx.fillStyle = "black";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.paddleLeft.draw(this.ctx, this.canvas.height);
@@ -142,11 +144,10 @@ var Game = /** @class */ (function () {
         this.ball.draw(this.ctx, this.canvas.height);
         this.ctx.fillStyle = "white";
         this.ctx.font = "20px Arial";
-        this.ctx.fillText("".concat(this.scoreLeft, " - ").concat(this.scoreRight), this.canvas.width / 2 - 20, 30);
-    };
-    return Game;
-}());
-var canvas = document.getElementById("pongCanvas");
+        this.ctx.fillText(`${this.scoreLeft} - ${this.scoreRight}`, this.canvas.width / 2 - 20, 30);
+    }
+}
+const canvas = document.getElementById("pongCanvas");
 canvas.width = 800;
 canvas.height = 400;
 new Game(canvas);
