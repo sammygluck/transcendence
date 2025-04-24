97aaa5ec9f503caac0d87c6ba33ccaeca1fd736e
jnijs <jnijs@student.s19.be>
2025-04-24 14:43:21 +0200

    tests

--- backend/server.js
+++ backend/server.js
@@ -169,7 +169,7 @@ fastify.register(async function (fastify) {
 });
 
 // Run the server!
-fastify.listen({ port: 3000 }, (err) => {
+fastify.listen({ port: 3000, /*host: "0.0.0.0"*/ }, (err) => {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
